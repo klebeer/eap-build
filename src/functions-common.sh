0453fed6428849c607c5d78416476785779007fa
Kleber Ayala <kleber.ayala@gmail.com>
2018-08-25 13:39:31 -0500

    fix: sed on macosx does not work

--- src/functions-common.sh
+++ src/functions-common.sh
@@ -56,9 +56,14 @@ function download_md5 {
 
     if [[ "$URL" == *"ftp.redhat.com"* ]]
     then
+      SED="sed -i"
+      if [[ "$OSTYPE" == "darwin"* ]]; 
+      then
+        SED="sed -i ''"
+      fi
         wget --output-file=$BUILD_HOME/work/build.log -O download/$FILENAME.md5 $DIR_URL/MD5SUM || STATUS=$?
-        sed -i "s/$FILENAME/download\/$FILENAME/g" download/$FILENAME.md5
-        sed -i "/$FILENAME/!d" download/$FILENAME.md5
+        SED "s/$FILENAME/download\/$FILENAME/g" download/$FILENAME.md5
+        SED "/$FILENAME/!d" download/$FILENAME.md5
     else
         wget --output-file=$BUILD_HOME/work/build.log -O download/$FILENAME.md5 $DIR_URL/$FILENAME.md5 || STATUS=$?
         echo "  download/$FILENAME" >> download/$FILENAME.md5 
